006bc4dd6c1075ec7c28cf8da334c1b70e369108
allengt2002 <97087349+allengt2002@users.noreply.github.com>
2024-10-08 15:03:15 +1100

    Add files via upload

---
+++ Allen-sit722-part 3/7.2P-resources/project_part3/book_catalog/app/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+SQLALCHEMY_DATABASE_URL = "postgresql://allen_sit722_user:TWS2dg9t1BM0gxxHVHJYNCKmJZSlt2LD@dpg-cs1s16m8ii6s73d6u0e0-a.oregon-postgres.render.com/allen_sit722"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
